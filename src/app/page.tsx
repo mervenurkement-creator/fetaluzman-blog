3f03067845beb70c7f7de852f96f6b2d93cfd97b
mervenurkement-creator <mervenurkement@gmail.com>
2026-03-31 17:36:48 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { getAllPosts } from '@/lib/posts'
 import HomePage from '@/components/HomePage'
-
 export default function Page() {
   const posts = getAllPosts()
   return <HomePage posts={posts} />
