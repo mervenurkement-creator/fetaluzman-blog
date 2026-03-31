e8e9787e2c2174a48dfa6a531af127f05afd06a4
mervenurkement-creator <mervenurkement@gmail.com>
2026-03-31 15:01:46 +0300

    Update mdxConfig.ts

--- src/lib/mdxConfig.ts
+++ src/lib/mdxConfig.ts
@@ -1,19 +1,2 @@
-import remarkGfm from 'remark-gfm'
-import rehypeSlug from 'rehype-slug'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypePrettyCode from 'rehype-pretty-code'
-
-export const remarkPlugins: any[] = [remarkGfm]
-
-export const rehypePlugins: any[] = [
-  rehypeSlug,
-  [rehypeAutolinkHeadings, { behavior: 'wrap', properties: { className: ['anchor-heading'] } }],
-  [rehypePrettyCode, {
-    theme: 'one-dark-pro',
-    keepBackground: true,
-    defaultLang: 'plaintext',
-    onVisitLine(node: any) {
-      if (node.children.length === 0) node.children = [{ type: 'text', value: ' ' }]
-    },
-  }],
-]
+export const remarkPlugins: any[] = []
+export const rehypePlugins: any[] = []
